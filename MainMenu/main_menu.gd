bec20bd1e9f653fbeef3c8d566317d1ada697e2f
Sebastian Abel <sebastian-abel@outlook.de>
2024-11-04 21:07:49 +0100

    created simple main menu

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -4,12 +4,19 @@ extends Control
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton as Button
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton as Button
 @onready var settings_button = $MarginContainer/HBoxContainer/VBoxContainer/SettingsButton as Button
-@export var start_level = preload("res://MainMenu/main_menu.tscn")
+@export var start_level = preload("res://game.tscn") as PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
 
+
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+	
+func on_exit_pressed() -> void:
+	get_tree().quit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
