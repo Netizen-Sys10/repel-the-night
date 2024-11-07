c5f0edfc31afc0c97676096eeab16fa60903ea3b
Sebastian Abel <sebastian-abel@outlook.de>
2024-11-07 20:49:55 +0100

    added OptionsMenu

---
+++ MainMenu/options_menu.gd
@@ -0,0 +1,20 @@
+class_name OptionsMenu
+extends Control
+
+@onready var exit_button = $MarginContainer/VBoxContainer/Exit_Button as Button
+
+
+signal exit_options_menu
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	exit_button.button_down.connect(on_exit_pressed)
+	set_process(false)
+
+func on_exit_pressed() -> void:
+	exit_options_menu.emit()
+	set_process(false)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
