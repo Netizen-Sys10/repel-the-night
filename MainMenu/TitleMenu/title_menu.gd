c67e2cc8569b50a32dda06a902d994bde94af3a8
Sebastian Abel <sebastian-abel@outlook.de>
2024-11-10 19:08:49 +0100

    updated Controls Menu

--- MainMenu/TitleMenu/title_menu.gd
+++ MainMenu/TitleMenu/title_menu.gd
@@ -6,7 +6,9 @@ extends Control
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton as Button
 @onready var options_button = $MarginContainer/HBoxContainer/VBoxContainer/OptionsButton as Button
 @onready var margin_container = $MarginContainer as MarginContainer
+@onready var controls_button = $MarginContainer/HBoxContainer/VBoxContainer/ControlsButton as Button
 @export var start_level = preload("res://Game/game.tscn") as PackedScene
+
 
 func enter() -> void:
 	pass
@@ -24,6 +26,9 @@ func on_start_pressed() -> void:
 func on_options_pressed() -> void:
 	if get_parent().has_method("change_to"):
 		get_parent().change_to("OptionsMenu")
+
+func on_controls_pressed() -> void:
+	pass
 	
 func on_exit_pressed() -> void:
 	get_tree().quit()
@@ -32,4 +37,5 @@ func on_exit_pressed() -> void:
 func handle_connecting_signals() -> void:
 	start_button.button_down.connect(on_start_pressed)
 	options_button.button_down.connect(on_options_pressed)
+	controls_button.button_down.connect(on_controls_pressed)
 	exit_button.button_down.connect(on_exit_pressed)
