755213b4a8153d6017cb14fdbfdf7e55498fe034
Sebastian Abel <sebastian-abel@outlook.de>
2024-11-18 21:45:23 +0100

    updated Player Animation

--- Units/Player/player.gd
+++ Units/Player/player.gd
@@ -1,17 +1,36 @@
-class_name Player extends CharacterBody2D
+extends CharacterBody2D
 
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var sprite : Sprite2D = $Sprite2D
 
-const SPEED = 300.0
-
+var speed = 100.0
+var direction : Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_vector("move_left", "move_right", "up", "down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction.x * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	update_animation()
+	update_facing_direction()
+	
+	
+func update_animation():
+	
+	animation_tree.set("parameters/Walk/blend_position", direction.x)
+	
+	
+func update_facing_direction():
+	
+	#Flip Sprite and set correct offset (player is not centered)
+	if direction.x > 0:
+		sprite.flip_h = false
+		sprite.offset.x = 17
+	elif direction.x < 0:
+		sprite.flip_h = true
+		sprite.offset.x = -17
